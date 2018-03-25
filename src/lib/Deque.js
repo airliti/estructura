f7ed15433fc60932d6b9a3a53f428eba5d9db8e3
airliti <airliti@yandex.com>
2018-03-25 14:59:49 +0200

    Deque

--- src/lib/Deque.js
+++ src/lib/Deque.js
@@ -7,53 +7,19 @@ class Deque {
      *
      * @example
      *
-     * const Deque = require('estructura/deque')
+     * const Deque = require('structural/deque')
      *
-     * //
-     * // Create a new instance of the Double-Ended Queue:
      * const aDeque = new Deque()
      */
     constructor() {
-        /**
-         * @private
-         *
-         * @type {Array.<*>}
-         */
-        this._fCollection = []
+        this.pushCollection_ = []
+        this.shftCollection_ = []
 
-        /**
-         * @private
-         *
-         * @type {Number}
-         */
-        this._fOffset = 0
+        this.pushOffset_ = 0
+        this.shftOffset_ = 0
 
-        /**
-         * @private
-         *
-         * @type {Array.<*>}
-         */
-        this._sCollection = []
-
-        /**
-         * @private
-         *
-         * @type {Number}
-         */
-        this._sOffset = 0
-
-        /**
-         * The total size of the Deque.
-         *
-         * @type {Number}
-         */
         this.size = 0
 
-        /**
-         * @see #.peek
-         *
-         * @type {Function}
-         */
         this.peekFront = this.peek
     }
 
@@ -74,7 +40,7 @@ class Deque {
      * >>> 1
      */
     push(aValue) {
-        this._sCollection.push(aValue)
+        this.pushCollection_.push(aValue)
 
         return ++this.size
     }
@@ -96,7 +62,7 @@ class Deque {
      * >>> 1
      */
     unshift(aValue) {
-        this._fCollection.push(aValue)
+        this.shftCollection_.push(aValue)
 
         return ++this.size
     }
@@ -105,7 +71,7 @@ class Deque {
      * Return the last item within the Deque.
      *
      * Maximum of `O(n)` due to the internal structure used to store the Double-Ended
-     * Queue and the built-in `#.slice` Function within JavaScript.
+     * Queue and the built-in `#.splice` Function within JavaScript.
      *
      * @return {*} Return `undefined` when the `#.size` is equal to 0.
      *
@@ -118,99 +84,28 @@ class Deque {
      * >>> Hello John Doe!
      */
     pop() {
-        // return this.popOrShift('_sCollection', '_sOffset', '_fCollection', '_fOffset')
-
-        //
-        // this._fOffset = 0
-        // this._fCollection = [ ]
-        // 
-        // this._sOffset = 2
-        // this._sCollection = [ null, null, "A", "B", "C" ]
-        // 
-        // +---+---+---+---+---+
-        // | 0 | 1 | 2 | 3 | 4 |
-        // +---+---+---+---+---+
-        // | N | N | A | B | C |
-        // +---+---+---+---+---+
-        // 
-        // .shift() -> Position 0 became "null"
-        // .shift() -> Position 1 became "null"
-        // 
-        // .push('A')
-        // .push('B')
-        // .push('C')
-        // 
-        // N: null
-        // 
-        // -> The #.shift method was used twice increasing the #._sOffset to 2. But
-        // unless that value is equal to the length of the Array, we can still
-        // #.pop an item of the back.
-        //
-        if (this._sOffset < this._sCollection.length) {
+        if (this.pushOffset_ < this.pushCollection_.length) {
             this.size--
 
-            return this._sCollection.pop()
+            return this.pushCollection_.pop()
         }
 
-        //
-        // this._fOffset = 2
-        // this._fCollection = [ null, null, "A", "B", "C" ]
-        // 
-        // this._sOffset = 0
-        // this._sCollection = [ ]
-        // 
-        // +---+---+---+---+---+
-        // | 0 | 1 | 2 | 3 | 4 |
-        // +---+---+---+---+---+
-        // | N | N | A | B | C |
-        // +---+---+---+---+---+
-        // 
-        // .pop() -> Position 0 became "null"
-        // .pop() -> Position 1 became "null"
-        // 
-        // .unshift('A')
-        // .unshift('B')
-        // .unshift('C')
-        // 
-        // N: null
-        // 
-        // -> In our case, the #.pop method was used twice increasing the
-        // #._fOffset to 2. But unless that value is equal to the length of
-        // the Array, we can still #.pop an item of the back.
-        //
-        if (this._fOffset < this._fCollection.length) {
+        if (this.shftOffset_ < this.shftCollection_.length) {
             this.size--
 
-            //
-            // @type {*}
-            //
-            const poppedItem = this._fCollection[this._fOffset]
+            const aValue = this.shftCollection_[this.shftOffset_]
 
-            this._fCollection[this._fOffset] = null
+            this.shftCollection_[this.shftOffset_] = null
 
-            this._fOffset++
+            this.shftOffset_++
 
-            //
-            // this._fOffset = 2
-            // this._fCollection = [ null, null, "A", "B", "C" ]
-            // 
-            // +---+---+---+---+---+
-            // | 0 | 1 | 2 | 3 | 4 |
-            // +---+---+---+---+---+
-            // | N | N | A | B | C |
-            // +---+---+---+---+---+
-            // 
-            // N: null
-            // 
-            // -> Resize the Array when +50 % of the Array is "null";
-            //
-            if (this._fOffset * 2 >= this._fCollection.length) {
-                this._fCollection = this._fCollection.slice(this._fOffset)
+            if (this.shftOffset_ << 1 >= this.shftCollection_.length) {
+                this.shftCollection_.splice(0, this.shftOffset_)
 
-                this._fOffset = 0
+                this.shftOffset_ = 0
             }
 
-            return poppedItem
+            return aValue
         }
 
         return undefined
@@ -220,7 +115,7 @@ class Deque {
      * Return the first item within the Double-Ended Queue.
      *
      * Maximum of `O(n)` due to the resizing of the Deque when necessary due to
-     * the internal structure. Is dependent on the `#.slice` Function within
+     * the internal structure. Is dependent on the `#.splice` Function within
      * JavaScript.
      *
      * @return {*} Return the first item within the Deque or `undefined` when the
@@ -235,104 +130,28 @@ class Deque {
      * >>> Hello Jane Doe!
      */
     shift() {
-        // return this.popOrShift('_fCollection', '_fOffset', '_sCollection', '_sOffset')
-
-        //
-        // this._fOffset = 2
-        // this._fCollection = [ null, null, "A", "B", "C" ]
-        // 
-        // this._sOffset = 0
-        // this._sCollection = [ ]
-        // 
-        // -> The #._sOffset and #._sCollection aren't taken into account yet, because
-        // there is something within the #._fCollection to return.
-        // 
-        // +---+---+---+---+---+
-        // | 0 | 1 | 2 | 3 | 4 |
-        // +---+---+---+---+---+
-        // | N | N | A | B | C |
-        // +---+---+---+---+---+
-        // 
-        // .pop() -> Position 0 became "null"
-        // .pop() -> Position 1 became "null"
-        // 
-        // .unshift('A') -> Inserted A at position 2
-        // .unshift('B') -> Put B at position 3
-        // .unshift('C') -> Append C to position 4
-        // 
-        // N: null
-        // 
-        // -> If the #._fOffset is smaller than the Array length, we can assume
-        // there is an item to #.pop. Why #.pop instead of #.shift? Mainly for
-        // performance.
-        //
-        if (this._fOffset < this._fCollection.length) {
+        if (this.shftOffset_ < this.shftCollection_.length) {
             this.size--
 
-            return this._fCollection.pop()
+            return this.shftCollection_.pop()
         }
 
-        //
-        // this._fOffset = 0
-        // this._fCollection = [ ]
-        // 
-        // -> The #._fOffset and #._fCollection does NOT matter at the moment, it
-        // has been handled.
-        // 
-        // this._sOffset = 2
-        // this._sCollection = [ null, null, "A", "B", "C" ]
-        // 
-        // +---+---+---+---+---+
-        // | 0 | 1 | 2 | 3 | 4 |
-        // +---+---+---+---+---+
-        // | N | N | A | B | C |
-        // +---+---+---+---+---+
-        // 
-        // .shift() -> Position 0 became "null"
-        // .shift() -> Position 1 became "null"
-        // 
-        // .push('A')
-        // .push('B')
-        // .push('C')
-        // 
-        // N: null
-        // 
-        // -> We've used the #.shift method twice, and therefore we've increased
-        // the #._sOffset to 2.
-        //
-        if (this._sOffset < this._sCollection.length) {
+        if (this.pushOffset_ < this.pushCollection_.length) {
             this.size--
 
-            //
-            // @type {*}
-            //
-            const shiftedItem = this._sCollection[this._sOffset]
+            const aValue = this.pushCollection_[this.pushOffset_]
 
-            this._sCollection[this._sOffset] = null
+            this.pushCollection_[this.pushOffset_] = null
 
-            this._sOffset++
+            this.pushOffset_++
 
-            //
-            // this._sOffset = 2
-            // 
-            // +---+---+---+---+---+
-            // | 0 | 1 | 2 | 3 | 4 |
-            // +---+---+---+---+---+
-            // | N | N | A | B | C |
-            // +---+---+---+---+---+
-            // 
-            // N: null
-            // 
-            // -> Clear the Array of "null" values when +50 % of the values
-            // within the Array are "null";
-            //
-            if (this._sOffset * 2 >= this._sCollection.length) {
-                this._sCollection = this._sCollection.slice(this._sOffset)
+            if (this.pushOffset_ << 1 >= this.pushCollection_.length) {
+                this.pushCollection_.splice(0, this.pushOffset_)
 
-                this._sOffset = 0
+                this.pushOffset_ = 0
             }
 
-            return shiftedItem
+            return aValue
         }
 
         return undefined
@@ -353,17 +172,10 @@ class Deque {
      *
      * aDeque.peek()
      * >>> Hello John Doe!
-     *
-     * @alias peekFront
      */
     peek() {
-        if (this._fCollection.length > this._fOffset) {
-            return this._fCollection[this._fCollection.length - 1]
-        }
-
-        if (this._sCollection.length > this._sOffset) {
-            return this._sCollection[this._sOffset]
-        }
+        if (this.shftCollection_.length > this.shftOffset_) return this.shftCollection_[this.shftCollection_.length - 1]
+        if (this.pushCollection_.length > this.pushOffset_) return this.pushCollection_[this.pushOffset_]
 
         return undefined
     }
@@ -398,13 +210,8 @@ class Deque {
      * >>> Hello A.
      */
     peekBack() {
-        if (this._sCollection.length > this._sOffset) {
-            return this._sCollection[this._sCollection.length - 1]
-        }
-
-        if (this._fCollection.length > this._fOffset) {
-            return this._fCollection[this._fOffset]
-        }
+        if (this.pushCollection_.length > this.pushOffset_) return this.pushCollection_[this.pushCollection_.length - 1]
+        if (this.shftCollection_.length > this.shftOffset_) return this.shftCollection_[this.shftOffset_]
 
         return undefined
     }
@@ -422,9 +229,12 @@ class Deque {
      * >>> [ "Hello John Doe!", "Hello Jane Doe!" ]
      */
     toArray() {
-        const toArray = this._fCollection.slice(this._fOffset).reverse()
+        const toArray = []
 
-        return toArray.concat(this._sCollection.slice(this._sOffset))
+        for (let idxNr = this.shftCollection_.length - 1, minNr = this.shftOffset_ - 1; idxNr > minNr; idxNr--) toArray.push(this.shftCollection_[idxNr])
+        for (let idxNr = this.pushOffset_ - 0, maxNr = this.pushCollection_.length - 0; idxNr < maxNr; idxNr++) toArray.push(this.pushCollection_[idxNr])
+
+        return toArray
     }
 
     /**
